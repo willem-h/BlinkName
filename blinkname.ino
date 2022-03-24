d03363950ebd2c06c37d0b3555ff7d1dc0498fec
Willem Handreck <willem-h@users.noreply.github.com>
2022-03-24 21:16:45 +1000

    Blink "Willem" in Morse code (#1)
    It's been formatted this way to read like Morse code would be said for each character. This is because Morse code is new to me and it helped me to ensure it was done correctly with just a visual check.

--- blinkname.ino
+++ blinkname.ino
@@ -1,8 +1,18 @@
-// Example code by Particle which can be found at
-// https://docs.particle.io/tutorials/hardware-projects/hardware-examples/#code
-
 // Pin for LED that will be blinked.
 const pin_t LED = D7;
+
+// Morse Code dot and dash durations.
+// https://morsecode.world/international/morse2.html
+//
+// An extra MORSE_UNIT is subtracted because it is included in the dot and dash
+// functions while still making it clear that they're defined the same as the
+// standard.
+const int MORSE_UNIT = 100;
+const int DOT_DURATION = MORSE_UNIT;
+const int DASH_DURATION = MORSE_UNIT * 3;
+const int BETWEEN_COMPONENT_DURATION = MORSE_UNIT;
+const int BETWEEN_CHARACTER_DURATION = MORSE_UNIT * 3 - MORSE_UNIT;  
+const int BETWEEN_WORD_DURATION = MORSE_UNIT * 7 - MORSE_UNIT;
 
 void setup()
 {
@@ -11,8 +21,47 @@ void setup()
 
 void loop()
 {
+  // My name, Willem, is going to be blinked in Morse Code.
+
+  // W: .--
+  dot(); dash(); dash();
+  delay(BETWEEN_CHARACTER_DURATION);
+
+  // I: ..
+  dot(); dot();
+  delay(BETWEEN_CHARACTER_DURATION);
+
+  // L: .-..
+  dot(); dash(); dot(); dot();
+  delay(BETWEEN_CHARACTER_DURATION);
+
+  // L: .-..
+  dot(); dash(); dot(); dot();
+  delay(BETWEEN_CHARACTER_DURATION);
+
+  // E: .
+  dot();
+  delay(BETWEEN_CHARACTER_DURATION);
+
+  // M: --
+  dash(); dash();
+  delay(BETWEEN_CHARACTER_DURATION);
+
+  delay(BETWEEN_WORD_DURATION);
+}
+
+void dot()
+{
   digitalWrite(LED, HIGH);
-  delay(1s);
+  delay(DOT_DURATION);
   digitalWrite(LED, LOW);
-  delay(1s);
+  delay(BETWEEN_COMPONENT_DURATION);
+}
+
+void dash()
+{
+  digitalWrite(LED, HIGH);
+  delay(DASH_DURATION);
+  digitalWrite(LED, LOW);
+  delay(BETWEEN_COMPONENT_DURATION);
 }
