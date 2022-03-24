28cdf62c2c8f1904ecb1b2de241d7377ea7c8cea
Willem Handreck <willem.handreck@gmail.com>
2022-03-24 21:01:53 +1000

    Remove duplication of delay

--- blinkname.ino
+++ blinkname.ino
@@ -3,12 +3,16 @@ const pin_t LED = D7;
 
 // Morse Code dot and dash durations.
 // https://morsecode.world/international/morse2.html
+//
+// An extra MORSE_UNIT is subtracted because it is included in the dot and dash
+// functions while still making it clear that they're defined the same as the
+// standard.
 const int MORSE_UNIT = 100;
 const int DOT_DURATION = MORSE_UNIT;
 const int DASH_DURATION = MORSE_UNIT * 3;
 const int BETWEEN_COMPONENT_DURATION = MORSE_UNIT;
-const int BETWEEN_CHARACTER_DURATION = MORSE_UNIT * 3;
-const int BETWEEN_WORD_DURATION = MORSE_UNIT * 7;
+const int BETWEEN_CHARACTER_DURATION = MORSE_UNIT * 3 - MORSE_UNIT;  
+const int BETWEEN_WORD_DURATION = MORSE_UNIT * 7 - MORSE_UNIT;
 
 void setup()
 {
@@ -20,19 +24,19 @@ void loop()
   // My name, Willem, is going to be blinked in Morse Code.
 
   // W: .--
-  dot(); delay(BETWEEN_COMPONENT_DURATION); dash(); delay(BETWEEN_COMPONENT_DURATION); dash();
+  dot(); dash(); dash();
   delay(BETWEEN_CHARACTER_DURATION);
 
   // I: ..
-  dot(); delay(BETWEEN_COMPONENT_DURATION); dot();
+  dot(); dot();
   delay(BETWEEN_CHARACTER_DURATION);
 
   // L: .-..
-  dot(); delay(BETWEEN_COMPONENT_DURATION); dash(); delay(BETWEEN_COMPONENT_DURATION); dot(); delay(BETWEEN_COMPONENT_DURATION); dot();
+  dot(); dash(); dot(); dot();
   delay(BETWEEN_CHARACTER_DURATION);
 
   // L: .-..
-  dot(); delay(BETWEEN_COMPONENT_DURATION); dash(); delay(BETWEEN_COMPONENT_DURATION); dot(); delay(BETWEEN_COMPONENT_DURATION); dot();
+  dot(); dash(); dot(); dot();
   delay(BETWEEN_CHARACTER_DURATION);
 
   // E: .
@@ -40,7 +44,7 @@ void loop()
   delay(BETWEEN_CHARACTER_DURATION);
 
   // M: --
-  dash(); delay(BETWEEN_COMPONENT_DURATION); dash();
+  dash(); dash();
   delay(BETWEEN_CHARACTER_DURATION);
 
   delay(BETWEEN_WORD_DURATION);
@@ -51,6 +55,7 @@ void dot()
   digitalWrite(LED, HIGH);
   delay(DOT_DURATION);
   digitalWrite(LED, LOW);
+  delay(BETWEEN_COMPONENT_DURATION);
 }
 
 void dash()
@@ -58,4 +63,5 @@ void dash()
   digitalWrite(LED, HIGH);
   delay(DASH_DURATION);
   digitalWrite(LED, LOW);
+  delay(BETWEEN_COMPONENT_DURATION);
 }
